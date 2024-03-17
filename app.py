9547d32e507cefdb870d29897db8ecc421b1a640
Erick-Andrade <erickprog12@gmail.com>
2024-03-17 00:37:07 -0300

    Conexão com AI

--- app.py
+++ app.py
@@ -1,47 +1,65 @@
-### Import Packages
-import time
+import requests
 import oci
-import json
-import re
-
+from flask import Flask
 from oci.ai_vision import AIServiceVisionClient
-from oci.ai_vision.models.create_image_job_details import CreateImageJobDetails
-from oci.ai_vision.models.image_object_detection_feature import ImageObjectDetectionFeature
-from oci.ai_vision.models.input_location import InputLocation
-from oci.ai_vision.models.object_list_inline_input_location import ObjectListInlineInputLocation
-from oci.ai_vision.models.object_location import ObjectLocation
-from oci.ai_vision.models.object_storage_document_details import ObjectStorageDocumentDetails
-from oci.ai_vision.models.output_location import OutputLocation
-from oci.object_storage import ObjectStorageClient
-
-### Define Variables
-namespace_name = "ModeloAlgodaoDefinitivo" 
-bucket_name = "bucketPragas"
-compartment_id = "ocid1.tenancy.oc1..aaaaaaaa2coodpgxxxqrtjmobsbz6jesjaghapg3jo5fxyz5gwpsfv4laxsa"
-input_prefix = "Sample-Images"
-output_prefix = "Final-Results"
-max_results_per_image = 25
-
-# Auth Config Definition
-config = oci.config.from_file('.oci/config')
-
-# AI Vision Client Definition
-ai_vision_client = oci.ai_vision.AIServiceVisionClient(config)
-
-### Get Images from Object Storage Using Object Storage Client
-# List Objects in Bucket
-object_storage_client = ObjectStorageClient(config)
+from oci.ai_vision.models import ImageClassificationFeature, AnalyzeImageDetails, InlineImageDetails
 
 
-### Get Images from Object Storage Using Object Storage Client
-# List Objects in Bucket
-object_list = object_storage_client.list_objects(
+app = Flask(__name__)
+namespace_name = "grkjou41lac1" 
+bucket_name = "pidaydemo"
+
+config = oci.config.from_file('~/.oci/config')
+vision_client = AIServiceVisionClient(config)
+
+@app.route("/")
+def index():
+
+    return "Hello"
+
+@app.route("/upload", methods=['POST'])
+def upload(image):
+    if 'image' not in requests.files:
+        return "Nenhuma imagem encontrada", 400
     
-### Get Images from Object Storage Using Object Storage Client
-# List Objects in Bucket
-object_list = object_storage_client.list_objects(
-    namespace_name = namespace_name,
-    bucket_name = bucket_name,
-    prefix = input_prefix
-)
-)
+    image_file = requests.files['image']
+    image_file.save('imagem_recebida.jpg')
+
+    # Manda para a IA
+
+    return image_file
+
+
+    # Recebe seu resultado
+
+
+
+def classify_image(image_data):
+    model__id = "ocid1.oc1.sa-saopaulo1.amaaaaaafgrfu6aaaffadwrrvr3ldf6fddkb3t3f4uf422ednk3umzi4z67a"
+    image_content = open(image_data, 'rb').read()
+    compartment__id = "ocid1.bucket.oc1.sa-saopaulo-1.aaaaaaaaxh5kmaamuvafi4wkxv4d2q3v5dipzmnecxkumzdzeiozqs4djc3"
+    
+    analyze_image_response = vision_client.analyze_image(
+        analyze_image_details = AnalyzeImageDetails(
+            compartment_id=compartment__id,
+            features=[
+                ImageClassificationFeature(
+                    feature_type="IMAGE_CLASSIFICATION",
+                    model_id=model__id
+                )],
+            image=InlineImageDetails(
+                source="INLINE",
+                data=image_data)
+            )
+    )
+
+    
+    return analyze_image_response
+
+
+if __name__ == "__main__":
+    #image_file_path = "folha.jpg"
+    #classify_image(image_file_path)
+    #result = classify_image(image_file_path)
+    #print(result.data)
+    app.run(host="0.0.0.0", port=5555, debug=True)
